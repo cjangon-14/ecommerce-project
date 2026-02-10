69e003d0b8d8819eed4ab582d4de3a1d83fe0ee5
kjjuythg <angon.chrisjohn014@gmail.com>
2026-02-10 17:09:55 +0800

    Created multiple pages using react-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
+import { Routes,Route } from 'react-router'
 import './App.css'
 import HomePage from './components/HomePage'
 
 function App() {
 
   return (
-    <HomePage />
+    
+    <Routes>
+      <Route path = '/' element = {<HomePage />} /> 
+      <Route path='test' element={<div>Teest Page</div>} />
+      <Route path='*' element={<div>404 Not Found</div>} />
+      <Route path='/test/test' element={<div>Test Test Page</div>} />
+    </Routes>
+
   )
 }
 
